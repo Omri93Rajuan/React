6f5788389a5995bbf1987c26621f52f0f8996ead
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-11-09 19:12:57 +0200

    I react-router-dom

--- BCA/src/users/StarsUsers.tsx
+++ BCA/src/users/StarsUsers.tsx
@@ -14,7 +14,10 @@ export default function StarsUsers({ users }: Props) {
   return (
     <>
       <h1>הכוכבים של מיקי!</h1>
-      <div className="card-list">
+      <div
+        className="card-list"
+        style={{ backgroundColor: users.length === 5 ? "red" : "magenta" }}
+      >
         {users.map((user) => (
           <div key={user.id} className="card">
             <h3>{user.username}</h3>
@@ -25,6 +28,7 @@ export default function StarsUsers({ users }: Props) {
               alt={`${user.username}'s avatar`}
               style={{ width: "100px", height: "100px" }}
             />
+            <h1 style={{ display: users.length === 5 ? "block" : "none" }}></h1>
           </div>
         ))}
       </div>
