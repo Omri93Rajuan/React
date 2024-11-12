bdf93ff6d4ed068f09e9d6e11da8d49e637746a4
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-11-12 23:25:32 +0200

    User login logic and context

--- BCA/src/main.tsx
+++ BCA/src/main.tsx
@@ -1,19 +1,23 @@
-import { StrictMode } from "react";
+import { StrictMode, useContext } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
-import UserProvider from "./providers/UserProvider.tsx";
-import StarsProvider from "./providers/StarsProvider.tsx";
+import { BrowserRouter, Navigate } from "react-router-dom";
+
+import { AuthContext, AuthProvider } from "./providers/authProvider.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <StarsProvider>
-          <App />
-        </StarsProvider>
-      </UserProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
+
+const LoginRedirect = () => {
+  const authContext = useContext(AuthContext);
+  return authContext?.user ? <Navigate to="/" /> : <LoginPage />;
+};
--- BCA/src/router/AppRouter.tsx
+++ BCA/src/router/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Users from "../users/Users";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
 
 export default function AppRouter() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>Welcome Miki to the CLASS!!!</h1>} />
-
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/users/*" element={<Users />} />
         <Route path="*" element={<h1>404 Who AM I?</h1>} />
       </Routes>
